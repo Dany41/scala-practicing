ba76c984360d5471ec431e9eda4ddc523760446a
Daniil Minakov <dany.minakov@gmail.com>
2024-01-08 18:55:28 +0200

    fpis: Functional Data Structures (in progress)

---
+++ fpis/src/main/scala/org/fpis/Playbox.scala
@@ -0,0 +1,16 @@
+package org.fpis
+
+import org.fpis.examples.List
+import List.*
+
+object Playbox extends App:
+
+  @annotation.nowarn // Scala gives a hint here via a warning, so let's disable that
+  val result = List(1, 2, 3, 4, 5) match
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+
+  println(List.result)
